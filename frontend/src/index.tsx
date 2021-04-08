bbfdb8452c4c5d5b88a37673195dae2e0a8c3996
Jakub Kozłowski <kubukoz@gmail.com>
2021-04-08 03:29:24 +0200

    Remove initial stuff

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+const MyComp = () => <div></div>;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyComp />
   </React.StrictMode>,
   document.getElementById("root")
 );
