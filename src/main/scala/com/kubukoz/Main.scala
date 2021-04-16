b119da89b8db9b6addee087eca36518464790455
Jakub Kozłowski <kubukoz@gmail.com>
2021-04-16 02:40:00 +0200

    Extract routing

--- src/main/scala/com/kubukoz/Main.scala
+++ src/main/scala/com/kubukoz/Main.scala
@@ -3,10 +3,8 @@ package com.kubukoz
 import cats.ApplicativeThrow
 import cats.effect.IO
 import cats.effect.IOApp
-import cats.effect.MonadCancelThrow
 import cats.effect.implicits._
 import cats.effect.kernel.Async
-import cats.effect.kernel.MonadCancel
 import cats.effect.kernel.Resource
 import cats.implicits._
 import ciris.ConfigValue
@@ -15,73 +13,16 @@ import com.kubukoz.indexer.Indexer
 import com.kubukoz.ocr.OCR
 import com.kubukoz.pipeline.IndexPipeline
 import com.kubukoz.pipeline.IndexingQueue
-import com.kubukoz.shared.Path
-import io.circe.Codec
-import io.circe.generic.semiauto._
 import org.http4s.HttpRoutes
-import org.http4s.circe._
 import org.http4s.client
 import org.http4s.client.Client
 import org.http4s.client.blaze.BlazeClientBuilder
-import org.http4s.dsl.Http4sDsl
-import org.http4s.headers.`Content-Type`
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.CORS
 import org.http4s.server.middleware.{Logger => ServerLogger}
 import org.typelevel.log4cats.SelfAwareStructuredLogger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
-
-final case class IndexRequest(path: String)
-
-object IndexRequest {
-  implicit val codec: Codec.AsObject[IndexRequest] = deriveCodec
-}
-
-object Routing {
-
-  def routes[F[_]: MonadCancelThrow: JsonDecoder: Indexer: ImageSource](
-    indexingQueue: IndexingQueue[F, Path]
-  ): HttpRoutes[F] = {
-    object dsl extends Http4sDsl[F]
-    import dsl._
-    import io.circe.syntax._
-
-    object SearchQuery extends QueryParamDecoderMatcher[String]("query")
-
-    HttpRoutes.of {
-      case req @ POST -> Root / "index" =>
-        req.asJsonDecode[IndexRequest].flatMap { body =>
-          indexingQueue.offer(shared.Path(body.path))
-        } *> Accepted()
-
-      case GET -> Root / "search" :? SearchQuery(query) =>
-        Ok(Indexer[F].search(query).map(_.asJson))
-
-      case GET -> "view" /: rest =>
-        val getMetadataAndStream =
-          ImageSource[F].download(shared.Path(rest.segments.map(_.decoded()).mkString("/")))
-
-        //todo might be prone to race conditions
-        //todo caching etc. would be nice
-        //also content length
-        MonadCancel[F].uncancelable { poll =>
-          // We need the value of this resource later
-          // so we cancelably-allocate it, then hope for the best (that any cancelations on the request will shutdown this resource).
-          poll(getMetadataAndStream.allocated)
-            .map { case (fd, cancel) =>
-              fd.metadata -> Resource.pure(fd).onFinalize(cancel)
-            }
-            .flatMap { case (meta, file) =>
-              // probably don't need to pass poll anywhere here, since it's already out of scope...
-              Ok(fs2.Stream.resource(file).flatMap(_.content)).map(_.withContentType(`Content-Type`(meta.mediaType)))
-            }
-
-        }
-    }
-  }
-
-}
 
 // todo! checking if a file is already decoded and indexed, before trying to decode.
 // also, probably a UI form to index a path would be nice, and maybe an endpoint to see the progress (which path, how many files indexed, maybe running time), checking if a path was already indexed
---
+++ src/main/scala/com/kubukoz/Routing.scala
@@ -0,0 +1,67 @@
+package com.kubukoz
+
+import cats.effect.MonadCancelThrow
+import cats.effect.kernel.MonadCancel
+import cats.effect.kernel.Resource
+import cats.implicits._
+import com.kubukoz.imagesource.ImageSource
+import com.kubukoz.indexer.Indexer
+import com.kubukoz.pipeline.IndexingQueue
+import com.kubukoz.shared.Path
+import io.circe.Codec
+import io.circe.generic.semiauto._
+import org.http4s.HttpRoutes
+import org.http4s.circe._
+import org.http4s.dsl.Http4sDsl
+import org.http4s.headers.`Content-Type`
+
+final case class IndexRequest(path: String)
+
+object IndexRequest {
+  implicit val codec: Codec.AsObject[IndexRequest] = deriveCodec
+}
+
+object Routing {
+
+  def routes[F[_]: MonadCancelThrow: JsonDecoder: Indexer: ImageSource](
+    indexingQueue: IndexingQueue[F, Path]
+  ): HttpRoutes[F] = {
+    object dsl extends Http4sDsl[F]
+    import dsl._
+    import io.circe.syntax._
+
+    object SearchQuery extends QueryParamDecoderMatcher[String]("query")
+
+    HttpRoutes.of {
+      case req @ POST -> Root / "index" =>
+        req.asJsonDecode[IndexRequest].flatMap { body =>
+          indexingQueue.offer(shared.Path(body.path))
+        } *> Accepted()
+
+      case GET -> Root / "search" :? SearchQuery(query) =>
+        Ok(Indexer[F].search(query).map(_.asJson))
+
+      case GET -> "view" /: rest =>
+        val getMetadataAndStream =
+          ImageSource[F].download(shared.Path(rest.segments.map(_.decoded()).mkString("/")))
+
+        //todo might be prone to race conditions
+        //todo caching etc. would be nice
+        //also content length
+        MonadCancel[F].uncancelable { poll =>
+          // We need the value of this resource later
+          // so we cancelably-allocate it, then hope for the best (that any cancelations on the request will shutdown this resource).
+          poll(getMetadataAndStream.allocated)
+            .map { case (fd, cancel) =>
+              fd.metadata -> Resource.pure(fd).onFinalize(cancel)
+            }
+            .flatMap { case (meta, file) =>
+              // probably don't need to pass poll anywhere here, since it's already out of scope...
+              Ok(fs2.Stream.resource(file).flatMap(_.content)).map(_.withContentType(`Content-Type`(meta.mediaType)))
+            }
+
+        }
+    }
+  }
+
+}
