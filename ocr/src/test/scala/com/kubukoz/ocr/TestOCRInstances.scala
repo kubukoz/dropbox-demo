78350de105fbba59d11fd4359715ec2bcb2bb9a4
Jakub Kozłowski <kubukoz@gmail.com>
2021-04-27 01:44:49 +0200

    Add comment

--- ocr/src/test/scala/com/kubukoz/ocr/TestOCRInstances.scala
+++ ocr/src/test/scala/com/kubukoz/ocr/TestOCRInstances.scala
@@ -2,6 +2,7 @@ package com.kubukoz.ocr
 
 object TestOCRInstances {
 
+  // decodeText("hello".getBytes) == "hello"
   def simple[F[_]](implicit SC: fs2.Compiler[F, F]): OCR[F] =
     _.through(fs2.text.utf8Decode[F]).compile.string
 
