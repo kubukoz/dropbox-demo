ec5ab1677d4e3c1327ad2066a622e88f32dbbd4c
Jakub Kozłowski <kubukoz@gmail.com>
2021-05-24 19:20:58 +0200

    Bump nixpkgs

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,10 +1,10 @@
 let
-  version = "29b0d4d0b600f8f5dd0b86e3362a33d4181938f9";
+  version = "17f061226736b76bcb4b807cf32f22fd61d81464";
 in
 import (
   fetchTarball {
     name = "nixpkgs-unstable-${version}";
     url = "https://github.com/NixOS/nixpkgs/archive/${version}.tar.gz";
-    sha256 = "10cafssjk6wp7lr82pvqh8z7qiqwxpnh8cswnk1fbbw2pacrqxr1";
+    sha256 = "1047v990my1r2dglk4n84i0wihcf5mndxnbyz8i8asv41ab3ac9i";
   }
 )
